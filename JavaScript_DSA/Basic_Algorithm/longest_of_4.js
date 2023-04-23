e72dd5ba1f2e10016fcd507d2ed386d90845d4c4
Archit Kandu <architkandu@hotmail.com>
2023-04-23 23:07:17 +0530

    Longest of 4

---
+++ JavaScript_DSA/Basic_Algorithm/longest_of_4.js
@@ -0,0 +1,9 @@
+function largestOfFour(arr) {
+    let newArr=[];
+    for(let i=0;i<4;i++){
+        temp=Math.max(arr[i][0],arr[i][1],arr[i][2],arr[i][3]);
+        newArr.push(temp);
+    }
+    return newArr;
+}
+console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
