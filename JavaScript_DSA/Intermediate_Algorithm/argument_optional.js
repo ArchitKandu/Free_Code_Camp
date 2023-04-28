3a43c9b43e6280e71830dcc6684d424be70c1d4c
Archit Kandu <architkandu@hotmail.com>
2023-04-28 12:16:24 +0530

    Argument Optional

---
+++ JavaScript_DSA/Intermediate_Algorithm/argument_optional.js
@@ -0,0 +1,9 @@
+function addTogether() {
+    const[a,b]=arguments;
+    if(typeof(a) !=='number') return undefined;
+    if(arguments.length===1)
+        return (b) => addTogether(a,b);
+    if(typeof(b)!=='number') return undefined;
+    return a+b;
+}
+console.log(addTogether(2,3));
